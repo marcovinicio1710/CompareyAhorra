2d66ca56ac370d3677dce711d56ada9975bea854
marcovinicio1710 <mvjc.2017@gmail.com>
2023-04-17 19:19:01 -0600

    imprimir cuando se cer4ge

--- reviews/management/commands/load_xlsx.py
+++ reviews/management/commands/load_xlsx.py
@@ -35,7 +35,7 @@ def leer_master_12_atributtes(file,atributtes):
 
 file=pathh+'/reviews/management/commands/tabla_precio_super_2023.xlsx'
 num=0
-
+total=0
 lista_prod=leer_master_12_atributtes(file,'Super,Categ,Producto,Precio,Espacio_5,Espacio_6,Precios_kg,Peso_kg,Precios_lt,Peso_lt,Precios_unid,Unid')
 now = datetime.now()
 now=str(now)
@@ -43,6 +43,7 @@ lista_now=now.split()
 now=lista_now[0]
 
 for i in lista_prod:
+    print(len(lista_producto=Productos_Super.objects.all()), 'total ya guardados en database')
     
     Super=i[0]
     Categ=i[1]
@@ -81,5 +82,7 @@ for i in lista_prod:
                     fecha=now)
     if created:
         num+=1
-    print (num,'creados')
+        print(num)
+    total+=1
+    print (total,'creados')
 print( 'se cargaron ',num,'en database')
