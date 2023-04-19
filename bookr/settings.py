6c88238a9c8d34f658181921c7fdb76268080909
marcovinicio1710 <mvjc.2017@gmail.com>
2023-04-18 20:59:44 -0600

    agregar dns

--- bookr/settings.py
+++ bookr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["192.168.0.18", "127.0.0.1",'192.168.0.14']
+ALLOWED_HOSTS = ['www.compareyahorra.com','compareyahorra.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME') 
 if RENDER_EXTERNAL_HOSTNAME:
