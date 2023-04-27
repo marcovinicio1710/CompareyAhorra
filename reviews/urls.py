dad88a5f69e4ef77495e6a635f5ee9ada441f0a9
marcovinicio1710 <mvjc.2017@gmail.com>
2023-04-27 11:01:45 -0600

    sitemap y que elija base dato de anteayer

--- reviews/urls.py
+++ reviews/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('cr/canasta-basica/', views.canastabasica, name='canasta-basica'),
     path('cr/recuperar-password/', views.recovery_password, name='recuperar-password'),
     path('cr/nuestros-servicios/', views.publicidad_interna, name='nuestros-servicios'),
-    path("sitemap.xml",sitemap,{"sitemaps": sitemaps},name="django.sitemap"),
+    path("sitemap.xml",views.site_map,name='site-map'),
     path('productos/<int:id>', views.post_producto, name='post'),
     path('cr/sobre-nos/', views.sobrenos, name='sobre-nos'),
    
