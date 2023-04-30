d48c33d2f7af41ebe9d7c1a8466d2d5e420308c1
marcovinicio1710 <131065261+marcovinicio1710@users.noreply.github.com>
2023-04-30 09:43:40 -0600

    Update views.py

--- reviews/views.py
+++ reviews/views.py
@@ -29,12 +29,12 @@ def index(request):
         else:
             hoy=lista_now[0]'''
         if horaa<14:
-            d = str(noww - timedelta(days=4))
+            d = str(noww - timedelta(days=5))
             lista_d=d.split()
             hoy=lista_d[0]
         else:
             ##hoy=lista_now[0]  original
-            d = str(noww - timedelta(days=4))
+            d = str(noww - timedelta(days=5))
             lista_d=d.split()
             hoy=lista_d[0]
 
@@ -213,12 +213,12 @@ def search_prod(request, searched='',categoria='all',price_kg='no',price_lt='no'
     lista_hora=hora.split(':')
     horaa=int(lista_hora[0])
     if horaa<14:
-        d = str(noww - timedelta(days=4))
+        d = str(noww - timedelta(days=5))
         lista_d=d.split()
         hoy=lista_d[0]
     else:
         ##hoy=lista_now[0]  original
-        d = str(noww - timedelta(days=4))
+        d = str(noww - timedelta(days=5))
         lista_d=d.split()
         hoy=lista_d[0]
 
